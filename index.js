82a387dd9bd073fb0de5fa05ebd6622ba4f7027b
ShawonECE <send2shawon@gmail.com>
2023-11-26 09:29:08 +0600

    certain level completed

--- index.js
+++ index.js
@@ -8,7 +8,13 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const admin = require("firebase-admin");
+const serviceAccount = require("./volunteer-network-fd681-firebase-adminsdk-jjjq2-7c4d2030b9.json");
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+  });
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5yhhqym.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
@@ -28,12 +34,84 @@ app.get('/', (req, res) => {
 app.get('/works', (req, res) => {
     const coll = db.collection("works");
     client.connect()
-    .then(() =>coll.find({}).toArray())
+    .then(() => coll.find({}).toArray())
     .then(result => {
         res.send(result);
     })
     .catch(error => {
         console.error(error);
+    })
+    .finally(() => client.close());
+});
+
+app.get('/registrations', (req, res) => {
+    const coll = db.collection("registration");
+    client.connect()
+    .then(() => coll.find({}).toArray())
+    .then(result => {
+        res.send(result);
+    })
+    .catch(error => {
+        console.error(error);
+    })
+    .finally(() => client.close());
+});
+
+app.get('/events', (req, res) => {
+    const bearer = req.headers.authorization;
+    const coll = db.collection("registration");
+    let userEmail;
+    if (bearer && bearer.startsWith('Bearer ')) {
+        const idToken = bearer.split(' ')[1];
+        admin.auth().verifyIdToken(idToken)
+        .then((decodedToken) => {
+            const tokenEmail = decodedToken.email;
+            if (tokenEmail === req.query.email) {
+                userEmail = tokenEmail;
+            }
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    }
+
+    client.connect()
+    .then(() => coll.find({email:userEmail}).toArray())
+    .then(result => {
+        res.send(result);
+        console.log(result);
+    })
+    .catch(error => {
+        console.error(error);
+    })
+    .finally(() => client.close());
+});
+
+app.post('/register', (req, res) => {
+    const info = req.body;
+    const coll = db.collection("registration");
+    client.connect()
+    .then(() => coll.insertOne(info))
+    .then(result => {
+        res.send(result);
+    })
+    .catch(error => {
+        console.error(error);
+    })
+    .finally(() => client.close());
+    console.log(info);
+});
+
+app.delete('/delete/:id', async (req, res) => {
+    const id = req.params.id;
+    const coll = db.collection("registration");
+    client.connect()
+    .then(() => coll.deleteOne({_id: new ObjectId(id)}))
+    .then(result => {
+      res.send(result);
+    })
+    .catch(error => {
+      console.error(error);
     })
     .finally(() => client.close());
 });
